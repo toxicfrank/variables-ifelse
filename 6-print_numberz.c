c326519d433e0ca3a1b908b463adfd0b37ef04d9
toxicfrank <ngugifrancis930@gmail.com>
2023-05-02 20:51:24 +0100

    practice

---
+++ 6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
